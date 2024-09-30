995e311745e4613e376287649ece882b77f0f10f
김진우 <kim.jinwoo2@joongang.co.kr>
2024-09-30 13:46:16 +0900

    feat: complete vue snippet

--- src/tests/App.vue
+++ src/tests/App.vue
@@ -8,30 +8,29 @@ function createVueShelfHook<T>(subscribe: Subscribe<T>) {
     callback: (store: ShelfStore<T>) => ShelfStore<V>
   ): Ref<V> => {
     const abortController = new AbortController();
-    let vueRefs: Ref<V>;
+    let vueRefs: Ref<V> = {} as Ref<V>;
     let shelf: ShelfStore<V> = {} as ShelfStore<V>;
 
     onUnmounted(() => {
       abortController.abort();
     });
 
+    watch(vueRefs, newValues => {
+      shelf.value = newValues;
+    });
+
     subscribe(shelfStore => {
       shelf = callback(shelfStore);
-      if (vueRefs) {
+      if (vueRefs.value) {
         vueRefs.value = shelf.value as V;
       } else {
-        vueRefs = ref(shelf.value) as V;
+        vueRefs = ref(shelf.value) as Ref<V>;
       }
 
       return abortController.signal;
     });
 
     vueRefs.value = shelf.value as V;
-    console.log('1', vueRefs);
-
-    watch(vueRefs, newValues => {
-      shelf.value = newValues;
-    });
 
     return vueRefs;
   };
@@ -48,16 +47,13 @@ const useProfileShelf = createVueShelfHook(subscribe);
 // @ts-ignore
 window.p = subscribe();
 
-const age = useProfileShelf<number>(store => {
-  return store.age;
-});
-
-console.log('AGE', age);
+const age = useProfileShelf<number>(store => store.age);
+const name = useProfileShelf<string>(store => store.name);
 
 const increment = () => {
   age.value += 1;
 }; </script>
 
 <template>
-  <button @click="increment">Count is: {{ age }}</button>
+  <button @click="increment">{{ name }} Count is: {{ age }}</button>
 </template>
