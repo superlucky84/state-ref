c6d5d909b3ffb821ceffe28157c5255a29c1260c
김진우 <kim.jinwoo2@joongang.co.kr>
2024-09-30 13:40:52 +0900

    feat: vue snippet type fixed

--- src/tests/App.vue
+++ src/tests/App.vue
@@ -4,25 +4,30 @@ import lenshelf from '@/index';
 import type { ShelfStore, Subscribe } from '@/index';
 
 function createVueShelfHook<T>(subscribe: Subscribe<T>) {
-  const useShelf = (callback: (store: ShelfStore<T>) => ShelfStore<T>) => {
+  const useShelf = <V>(
+    callback: (store: ShelfStore<T>) => ShelfStore<V>
+  ): Ref<V> => {
     const abortController = new AbortController();
-    let vueRefs: Ref<T> = {} as Ref<T>;
-    let shelf: ShelfStore<T> = {} as ShelfStore<T>;
+    let vueRefs: Ref<V>;
+    let shelf: ShelfStore<V> = {} as ShelfStore<V>;
 
     onUnmounted(() => {
       abortController.abort();
     });
 
-    subscribe(shelf => {
-      shelf = callback(shelf);
+    subscribe(shelfStore => {
+      shelf = callback(shelfStore);
       if (vueRefs) {
-        vueRefs.value = shelf.value as T;
+        vueRefs.value = shelf.value as V;
+      } else {
+        vueRefs = ref(shelf.value) as V;
       }
 
       return abortController.signal;
     });
 
-    vueRefs.value = shelf.value as T;
+    vueRefs.value = shelf.value as V;
+    console.log('1', vueRefs);
 
     watch(vueRefs, newValues => {
       shelf.value = newValues;
@@ -34,7 +39,7 @@ function createVueShelfHook<T>(subscribe: Subscribe<T>) {
   return useShelf;
 }
 
-const subscribe = lenshelf({
+const subscribe = lenshelf<{ name: string; age: number }>({
   name: 'brown',
   age: 13,
 });
@@ -43,14 +48,15 @@ const useProfileShelf = createVueShelfHook(subscribe);
 // @ts-ignore
 window.p = subscribe();
 
-const age = useProfileShelf(store => {
+const age = useProfileShelf<number>(store => {
   return store.age;
 });
 
+console.log('AGE', age);
+
 const increment = () => {
   age.value += 1;
-};
-</script>
+}; </script>
 
 <template>
   <button @click="increment">Count is: {{ age }}</button>
