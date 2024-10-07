aa9c2c25208d1af86c9e4b24a5421ce27074d871
superlucky84 <superlucky84@gmail.com>
2024-10-07 10:22:02 +0900

    feat: test 코드 작성중

--- packages/connect-preact/src/tests/preact/preact.tsx
+++ packages/connect-preact/src/tests/preact/preact.tsx
@@ -1,59 +1,91 @@
-import { render as trender, screen } from '@testing-library/preact';
+import {
+  render as trender,
+  fireEvent,
+  cleanup,
+  screen,
+} from '@testing-library/preact';
 import { h, render } from 'preact';
 import { createStore } from 'state-ref';
 import { connectWithPreactA } from '@/index';
 
-console.log(createStore);
-
-const watch = createStore<{ name: string; age: number }>({
-  name: 'brown',
+type Profile = { name: string; age: number };
+const getDefaultValue = () => ({
+  name: 'Brown',
   age: 13,
 });
-
+const watch = createStore<Profile>(getDefaultValue());
+const p = watch();
 const usePofileStore = connectWithPreactA(watch);
 
-const p = watch();
-
-//@ts-ignore
-window.p = p;
+const resetStore = () => {
+  p.value = getDefaultValue();
+};
 
 function Name() {
   const stateRef = usePofileStore();
 
-  return <div>aa = {stateRef.age.value}</div>;
+  return (
+    <div>
+      <div>name: {stateRef.name.value}</div>
+      <button
+        onClick={() =>
+          (stateRef.name.value = getRandomName(stateRef.name.value))
+        }
+      >
+        nameChange
+      </button>
+    </div>
+  );
 }
 
 function Age() {
-  const stateRef = usePofileStore();
+  const { age } = usePofileStore();
 
-  return <div>bb = {stateRef.name.value}</div>;
+  return (
+    <div>
+      <div data-testid="age-display">age: {age.value}</div>
+      <button data-testid="age-increase" onClick={() => (age.value += 1)}>
+        increase
+      </button>
+    </div>
+  );
 }
-
-if (!import.meta.vitest) {
-  render(<Age />, document.getElementById('root') as HTMLElement);
-  render(<Name />, document.getElementById('root2') as HTMLElement);
+function Root() {
+  return (
+    <div>
+      <Age />
+      <Name />
+    </div>
+  );
 }
 
 if (import.meta.vitest) {
   // const { describe, it, expect } = import.meta.vitest;
 
-  trender(<Age />);
-  trender(<Name />);
-
   describe('Connect Preact', () => {
-    it('프리미티브 타입의 값 하나에 대해 변경이 잘 반영되어야 한다.', () => {
-      expect(screen.getByText('aa = 13')).toBeInTheDocument();
-    });
-    /*
-    it('프리미티브 널타입으로 변경될때도 잘 반영되어야한다.', () => {
-      expect(screen.getByText('aa = 13')).toBeInTheDocument();
-    });
-    it('구조분해 할당하여 꺼내온 stateRef 값에 대해서도 잘 동작해야 한다.', () => {
-      expect(screen.getByText('aa = 13')).toBeInTheDocument();
+    it('꺼내온 stateRef 값에 대해서 리액티브하게 잘 동작해야 한다.', () => {
+      trender(<Age />);
+      const btnElement = screen.getByTestId('age-increase');
+      const displayElement = screen.getByTestId('age-display');
+
+      fireEvent.click(btnElement);
+      expect(displayElement.textContent).toBe('age: 14');
+
+      cleanup();
+      resetStore();
     });
     it('여러개의 컴포넌트중 value로 꺼내어 값을 구독중인 컴포넌트에 변경만 동작해야한다.', () => {
-      expect(screen.getByText('aa = 13')).toBeInTheDocument();
+      trender(<Age />);
+      const btnElement = screen.getByTestId('age-increase');
+      const displayElement = screen.getByTestId('age-display');
+
+      fireEvent.click(btnElement);
+      expect(displayElement.textContent).toBe('age: 14');
+
+      cleanup();
+      resetStore();
     });
+    /*
     it('언마운트된 컴포넌트에 구독함수 호출은 일어나지 않아야 한다.', () => {
       expect(screen.getByText('aa = 13')).toBeInTheDocument();
     });
@@ -65,4 +97,23 @@ if (import.meta.vitest) {
     });
      */
   });
+}
+
+function getRandomName(excludeName: string) {
+  const names = ['Brown', 'Alice', 'Bob', 'Charlie'];
+  const filteredNames = names.filter(name => name !== excludeName);
+
+  if (filteredNames.length === 0) {
+    throw new Error('No names left to choose from.');
+  }
+
+  const randomIndex = Math.floor(Math.random() * filteredNames.length);
+  return filteredNames[randomIndex];
+}
+
+if (!import.meta.vitest) {
+  //@ts-ignore
+  window.p = p;
+  // render(<Age />, document.getElementById('root') as HTMLElement);
+  render(<Name />, document.getElementById('root2') as HTMLElement);
 }
