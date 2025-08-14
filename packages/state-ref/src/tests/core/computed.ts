a1556ce3dea2e5cdf7337afe352477edf6f8a195
superlucky84 <superlucky84@gmail.com>
2025-08-14 19:02:34 +0900

    feat: added create computed test

--- packages/state-ref/src/tests/core/computed.ts
+++ packages/state-ref/src/tests/core/computed.ts
@@ -8,26 +8,43 @@ const makeDefaultValue2 = () => 111;
 const defaultValue1 = makeDefaultValue1();
 const defaultValue2 = makeDefaultValue2();
 
-const watch1 = createStore<number>(defaultValue1);
-const watch2 = createStore<number>(defaultValue2);
-
-
 /**
  * Auto Test
  */
 if (import.meta.vitest) {
   const { describe, it, expect, vi } = import.meta.vitest;
 
-  describe("Create computed", () => {
+  describe('Create computed', () => {
     it('The initial value returns the calculated value exactly.', () => {
-      const logSpy = vi.spyOn(console, 'log').mockImplementation(() => {});
-			const computedWatch = createComputed([watch1, watch2], ([ref1, ref2]) => {
-				return ref1.value + ref2.value;
-			});
+      const watch1 = createStore<number>(defaultValue1);
+      const watch2 = createStore<number>(defaultValue2);
 
-			const computedRef = computedWatch();
+      const computedWatch = createComputed([watch1, watch2], ([ref1, ref2]) => {
+        return ref1.value + ref2.value;
+      });
+
+      const computedRef = computedWatch();
 
       expect(computedRef.value).toBe(1111);
+    });
+
+    it('When the value changes, the change should be detected and the callback function should be executed.', () => {
+      const logSpy = vi.spyOn(console, 'log').mockImplementation(() => {});
+
+      const watch1 = createStore<number>(defaultValue1);
+      const watch2 = createStore<number>(defaultValue2);
+      const watch2Ref = watch2();
+
+      const computedWatch = createComputed([watch1, watch2], ([ref1, ref2]) => {
+        return ref1.value + ref2.value;
+      });
+
+      const computedRef = computedWatch(computedRef => {
+        console.log('value', computedRef.value);
+      });
+      watch2Ref.value += 111;
+
+      expect(logSpy).toHaveBeenCalledWith('value', 1222);
 
       logSpy.mockRestore();
     });
