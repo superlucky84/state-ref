7b321159c4380224532cf66766c34876418b633e
superlucky84 <superlucky84@gmail.com>
2025-09-29 19:06:17 +0900

    feat: added test

--- packages/state-ref/src/tests/core/combine.ts
+++ packages/state-ref/src/tests/core/combine.ts
@@ -5,11 +5,35 @@ import { createStore, combineWatch } from '@/index';
 const defaultValue1 = 1000;
 const defaultValue2 = { jj: 2000 };
 
-const watch1 = createStore<number>(defaultValue1);
-const watch2 = createStore<{ jj: number }>(defaultValue2);
+if (import.meta.vitest) {
+  const { describe, it, expect, vi } = import.meta.vitest;
 
-const combindWatch = combineWatch([watch1, watch2] as const);
+  describe('Create combineWatch', () => {
+    it('should immediately call callback when any watch value changes', () => {
+      const watch1 = createStore<number>(defaultValue1);
+      const watch2 = createStore<{ jj: number }>(defaultValue2);
 
-combindWatch(([ref1, ref2]) => {
-  console.log(ref1.value + ref2.jj.value);
-});
+      // mock callback
+      const callback = vi.fn();
+
+      const combinedWatch = combineWatch([watch1, watch2] as const);
+
+      // subscribe
+      combinedWatch(([ref1, ref2], isFirst) => {
+        callback(ref1.value + ref2.value.jj, isFirst);
+      });
+
+      // 초기 호출 확인
+      expect(callback).toHaveBeenCalledTimes(1);
+      expect(callback).toHaveBeenCalledWith(3000, true); // 1000 + 2000
+
+      watch1().value = 1500;
+      expect(callback).toHaveBeenCalledTimes(2);
+      expect(callback).toHaveBeenLastCalledWith(3500, false); // 1500 + 2000
+
+      watch2().value.jj = 2500;
+      expect(callback).toHaveBeenCalledTimes(3);
+      expect(callback).toHaveBeenLastCalledWith(4000, false); // 1500 + 2500
+    });
+  });
+}
